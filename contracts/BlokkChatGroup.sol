bfa654f8b3ef350518d829022375cfcdad7e0290
Tyler Brown <tbrown93@gatech.edu>
2017-09-23 18:20:07 -0400

    Worked on BlokkChatGroup

--- contracts/BlokkChatGroup.sol
+++ contracts/BlokkChatGroup.sol
@@ -16,7 +16,8 @@ contract BlokkChatGroup {
 
     struct Post {
         string title; //length limit enforced by ruleset, must be unique, immutable
-        uint256 number; //unique, immutable
+        uint256 number; //must be > 0 if post exists, unique, immutable
+        string contentType; //MIME-type of content
         IpfsMultihash contentAddress; //required only if enforced by ruleset
         address creator; //immutable
         uint256 creationTime; //UNIX timestamp, accept user input bounded by block.timestamp, immutable
@@ -26,14 +27,17 @@ contract BlokkChatGroup {
     }
 
     mapping (string => Post) postsByTitle;
-    mapping (address => Post[]) postsByCreator;
+    mapping (address => uint256[]) postNumbersByCreator;
     mapping (uint256 => Post) postsByNumber;
+
+    event NewPost(address indexed creator, uint256 indexed number, string title);
 
     uint256 userCount = 0;
 
     struct User {
         string nickname; //length/uniqueness enforced by ruleset
-        uint256 number; //unique, immutable
+        uint256 number; //must be > 0 if user exists, unique, immutable
+        string profileType; //MIME-type of profile
         IpfsMultihash profileAddress; //required only if enforced by ruleset
         address addr; //public key, unique, immutable
         uint256 joinTime; //UNIX timestamp when user first joined group, use block.timestamp, immutable
@@ -44,4 +48,113 @@ contract BlokkChatGroup {
 
     mapping (address => User) usersByAddress; //maps ethereum addres (public key) to user objects
     mapping (uint256 => User) usersByNumber;
+
+    function getPostByTitle(string _title) constant returns (
+        string title,
+        uint256 number,
+        string contentType,
+        IpfsMultihash contentAddress,
+        address creator,
+        uint256 creationTime,
+        address groupAddress,
+        uint256 balance,
+        int256 permissions
+    ) {
+        Post memory p = postsByTitle[_title];
+        return (
+            p.title,
+            p.number,
+            p.contentType,
+            p.contentAddress,
+            p.creator,
+            p.creationTime,
+            p.groupAddress,
+            p.balance,
+            p.permissions
+        );
+    }
+
+    function getPostByNumber(uint256 _number) constant returns (
+        string title,
+        uint256 number,
+        string contentType,
+        IpfsMultihash contentAddress,
+        address creator,
+        uint256 creationTime,
+        address groupAddress,
+        uint256 balance,
+        int256 permissions
+    ) {
+        Post memory p = postsByNumber[_number];
+        return (
+            p.title,
+            p.number,
+            p.contentType,
+            p.contentAddress,
+            p.creator,
+            p.creationTime,
+            p.groupAddress,
+            p.balance,
+            p.permissions
+        );
+    }
+
+    function getPostNumbersByCreator(address _creator) constant returns (uint256[]) {
+        return (postNumbersByCreator[_creator]);
+    }
+
+    function postExists(string title) returns (bool) {
+        return (getPostByTitle(title).number != 0);
+    }
+
+    function createPost(string title, string contentType, uint8 ipfsHashFunction, uint8 ipfsHashLength, bytes ipfsHash, uint256 creationTime) returns (uint256) {
+        //TODO: check title length via ruleset
+        //TODO: UTF-8 length != bytes().length
+        require(bytes(title).length <= 255);
+
+        require(!postExists(title));
+
+        require(ipfsHashLength != 0);
+
+        require(ipfsHashLength == ipfsHash.length);
+
+        //TODO: check if ipfs hash is 32 bytes long
+
+        uint256 ctLen = bytes(contentType).length;
+        require(ctLen > 0 && ctLen <= 255); //RFC 6838 limits mime types to 127 bytes for each of the major and minor types, plus the separating slash
+
+        if (creationTime > block.timestamp || creationTime <= (block.timestamp - 1 hours)) {
+            creationTime = block.timestamp;
+        }
+
+        address creator = msg.sender;
+
+        postCount++;
+
+        Post memory newPost = Post({
+            title: title,
+            number: postCount,
+            contentType: contentType,
+            contentAddress: IpfsMultihash({
+                hashFunction: ipfsHashFunction,
+                hashLength: ipfsHashLength,
+                hash: ipfsHash
+            }),
+            creator: creator,
+            creationTime: creationTime,
+            groupAddress: 0,
+            balance: 0, //TODO: default from ruleset
+            permissions: 0 //TODO: default from ruleset
+        });
+
+        postsByTitle[title] = newPost;
+        postsByCreator[creator].push(newPost);
+        postsByNumber[postCount] = newPost;
+
+        NewPost(creator, postCount, title);
+
+        return (postCount);
+    }
+
 }
+
